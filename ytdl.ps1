c447af6f66ed290782072991a83686a5be68d838
Andrey Tokarev <andrey777tokarev@yandex.ru>
2020-12-21 15:05:45 +0300

    fcommit

---
+++ ytdl.ps1
@@ -0,0 +1,21 @@
+$outputDir = "D:\video\"
+$archiveDir = "D:\mpv\downloaded.txt"
+
+$outputTemplatePlaylist = "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
+$outputTemplate = "%(title)s.%(ext)s"
+
+$script = "youtube-dl.exe"
+
+$link = $args[0]
+
+Function download([string]$url="",[bool]$isplaylist=$false) {
+    $output = $outputTemplate
+    
+    if ($isplaylist -eq $true) {
+        $output = $outputTemplatePlaylist
+    }
+
+    & $script --download-archive $archiveDir --no-overwrites --ignore-errors -o "$outputDir$output" $url
+}
+
+download $link (($link -like "*/playlist?*") -or ($link -like "*&list=*") -or ($link -like "*/playlists"))
