829e446eec25f4118336a576c41fb5ebf17e0427
Andrey Tokarev <andrey777tokarev@yandex.ru>
2021-09-26 15:51:59 +0300

    --no-download-archive

--- ytdl.ps1
+++ ytdl.ps1
@@ -9,9 +9,9 @@ $outputPlaylist = "%(playlist)s/%(playlist_index)s - "
 $url = $args[0] -replace "watch\?v=.*&list=", "playlist?list="
 $url = $url -replace "\?utm_source=player&utm_medium=video&utm_campaign=EMBED", ""
 
-$pltitle = cmd /c $ytdl --print playlist_title --no-mark-watched --playlist-end 1 $url
-$vuploader = cmd /c $ytdl --print uploader --no-mark-watched --playlist-end 1 $url
-$vid = cmd /c $ytdl --print id --no-mark-watched --playlist-end 1 $url
+$pltitle = cmd /c $ytdl --print playlist_title --no-download-archive --no-mark-watched --playlist-end 1 $url
+$vuploader = cmd /c $ytdl --print uploader --no-download-archive --no-mark-watched --playlist-end 1 $url
+$vid = cmd /c $ytdl --print id --no-download-archive --no-mark-watched --playlist-end 1 $url
 
 if (($pltitle -eq "Queue") -or ($pltitle -eq "Watch later")) {
     $output = "$uploader/%(playlist_index)s - $output"
