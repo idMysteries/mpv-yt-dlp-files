1a70c9c59de5d693143f17b8cab6433b69be47b6
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-01-28 20:50:51 +0300

    Twitch Clips

--- ytdl.ps1
+++ ytdl.ps1
@@ -1,7 +1,6 @@
 $ytdl = "yt-dlp.exe"
 $directory = "D:\video\"
 
-$uploader = "%(uploader)s"
 $archive = ""
 $output = "%(title)s.%(ext)s"
 $outputPlaylist = "%(playlist)s/%(playlist_index)s - "
@@ -9,9 +8,13 @@ $outputPlaylist = "%(playlist)s/%(playlist_index)s - "
 $url = $args[0] -replace "watch\?v=.*&list=", "playlist?list="
 $url = $url -replace "\?utm_source=player&utm_medium=video&utm_campaign=EMBED", ""
 
-$meta = cmd /c $ytdl --print "%(playlist_title)s <<>> %(uploader)s <<>> %(id)s" --no-download-archive --no-mark-watched --playlist-end 1 $url
+$meta = cmd /c $ytdl --print "%(playlist_id)s <<>> %(playlist_title)s <<>> %(uploader)s <<>> %(id)s" --no-download-archive --no-mark-watched --playlist-end 1 $url
 
-$pltitle, $vuploader, $vid = $meta -Split " <<>> "
+$plid, $pltitle, $vuploader, $vid = $meta -Split " <<>> "
+
+if ($url -match "twitch.tv/.*/clips") {
+    $vuploader = $plid
+}
 
 if (($pltitle -eq "Queue") -or ($pltitle -eq "Watch later")) {
     $output = "$uploader/%(playlist_index)s - $output"
@@ -22,7 +25,7 @@ else {
     }
 
     if ($vuploader -ne "NA") {
-        $output = $uploader + "/" + $output
+        $output = $vuploader + "/" + $output
     }
 
     if ($vid -eq "shell") {
