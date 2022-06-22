35e42f3c5237cf5a7a945535447cf8bab7613d44
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-06-22 15:44:53 +0300

    More args + id in output!

--- ytdl.ps1
+++ ytdl.ps1
@@ -5,13 +5,15 @@ $datedir = Get-Date -Format "\\dd.MM.yyyy\\"
 $uploader = "%(uploader)s"
 $archive = "--download-archive", "D:\mpv\downloaded.txt"
 $metatitle = "--parse-metadata", "title:%(meta_title)s"
-$output = "%(title)s.%(ext)s"
+$output = "%(title)s [%(id)s].%(ext)s"
 $outputPlaylist = "%(playlist)s/%(playlist_index)s - "
 
 & $ytdl --update
 
 $url = $args[0] -replace "watch\?v=.*&list=", "playlist?list="
 $url = $url -replace "\?utm_source=player&utm_medium=video&utm_campaign=EMBED", ""
+
+$null, $args = $args
 
 $meta = cmd /c $ytdl --print "%(playlist_id)s <<>> %(playlist_title)s <<>> %(uploader)s <<>> %(id)s <<>> %(extractor)s" --no-download-archive --no-mark-watched --playlist-end 1 $url
 
@@ -38,4 +40,4 @@ else {
     }
 }
 
-& $ytdl $archive $metatitle --concurrent-fragments 2 --live-from-start -o "$directory$datedir$output" $url
+& $ytdl $archive $metatitle $args --concurrent-fragments 2 --live-from-start -o "$directory$datedir$output" $url
