11e2d79319ba838c494de6e564a71684c451987e
Andrey Tokarev <andrey777tokarev@yandex.ru>
2024-08-13 18:51:49 +0300

    Update pause-when-minimize.lua

--- scripts/pause-when-minimize.lua
+++ scripts/pause-when-minimize.lua
@@ -1,15 +1,13 @@
 local was_minimized = false
 
-local function on_window_minimized(_, value)
-    if value then
-        was_minimized = true
+local function on_window_minimized(_, minimized)
+    if minimized then
         mp.set_property_native("pause", true)
-    else
-        if was_minimized then
-            mp.add_timeout(0.3, function()
-                mp.set_property_native("pause", false)
-            end)
-        end
+        was_minimized = true
+    elseif was_minimized then
+        mp.add_timeout(0.3, function()
+            mp.set_property_native("pause", false)
+        end)
         was_minimized = false
     end
 end
