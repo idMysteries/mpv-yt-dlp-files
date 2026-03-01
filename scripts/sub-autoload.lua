f526580238bc54cf977336066e86761fb6d97c62
Andrey Tokarev <andrey777tokarev@yandex.ru>
2026-03-01 15:10:44 +0300

    Update sub-autoload.lua

--- scripts/sub-autoload.lua
+++ scripts/sub-autoload.lua
@@ -79,12 +79,13 @@ local function episode_number(file, sorted_files)
 
     local function compare(i)
         local other_numbers = extract_numbers(sorted_files[i])
-        for n = 1, #numbers do
+        local len = math.min(#numbers, #other_numbers)
+        for n = 1, len do
             if numbers[n] ~= other_numbers[n] then
                 return numbers[n]
             end
         end
-        return numbers[1]
+        return nil
     end
 
     for i = idx + 1, #sorted_files do
